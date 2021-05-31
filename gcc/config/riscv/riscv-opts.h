5e0f1894635e0c91736f55b2cff35c824e804181
Lin Sinan <sinan@isrc.iscas.ac.cn>
2021-05-31 04:40:02 +0300

    Initial support for P extension with spec 0.96
    - initial support for p ext with spec 0.96
    - add instruction kabsw

--- gcc/config/riscv/riscv-opts.h
+++ gcc/config/riscv/riscv-opts.h
@@ -69,4 +69,10 @@ enum riscv_align_data {
 #define TARGET_ZKSED ((riscv_crypto_subext & MASK_ZKSED) != 0)
 #define TARGET_ZKSH ((riscv_crypto_subext & MASK_ZKSH) != 0)
 
+/* P extension subset */
+#define MASK_ZPN (1 << 0)
+#define MASK_ZPSF (1 << 1)
+
+#define TARGET_ZPN ((riscv_rvp_subext & MASK_ZPN) != 0)
+#define TARGET_ZPSF ((riscv_rvp_subext & MASK_ZPSF) != 0)
 #endif /* ! GCC_RISCV_OPTS_H */
